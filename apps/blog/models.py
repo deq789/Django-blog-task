81a2eb5bbd831db3a7301c913c75724b9f428009
Dennis <dequaderer@gmail.com>
2023-11-09 03:53:51 -0300

    Use slugify function
    Use slugify built-in function instead custom function because we are using the format {slug}-{article-id} in the url so unique slugs are not needed.

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,32 +1,20 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 from django.contrib.auth.models import User
 
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True, max_length=255, blank=True, null=True)
+    slug = models.SlugField(max_length=255, blank=True, null=True)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     publication_datetime = models.DateTimeField(default=timezone.now)
     is_online = models.BooleanField(default=False)
 
     def save(self, *args, **kwargs):
-        # Generate slug from title
-        self.slug = self.generate_unique_slug()
+        self.slug = slugify(self.title)
         super().save(*args, **kwargs)
-
-    def generate_unique_slug(self):
-        """
-        Generate a unique slug based on the article title.
-        """
-        slug = self.title.lower().replace(' ', '-')
-        unique_slug = slug
-        num = 2
-        while Article.objects.filter(slug=unique_slug).exists():
-            unique_slug = f"{slug}-{num}"
-            num += 1
-        return unique_slug
 
     def __str__(self):
         return f"{self.title} - {self.slug}"
