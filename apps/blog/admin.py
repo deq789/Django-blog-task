6500c56ca043940f8da735d69b6388f0c5bebd5f
Dennis <dequaderer@gmail.com>
2023-11-10 01:31:23 -0300

    Set article slug and publication_datetime as readonly fields

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -6,6 +6,7 @@ from .models import Article
 class ArticleAdmin(admin.ModelAdmin):
 
     list_display = ['title',  'author', 'is_online', 'publication_datetime']
+    readonly_fields = ('slug', 'publication_datetime')
 
 
 admin.site.register(Article, ArticleAdmin)
