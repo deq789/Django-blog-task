1e5567133b6e43c2c39a5aab23cc33726d164da9
Dennis <dequaderer@gmail.com>
2023-11-16 03:14:56 -0300

    Refactor send email to be used as part of utils module

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,10 +1,7 @@
-from django.core.mail import EmailMessage
-from django.conf import settings
 from django.views.generic.edit import FormView
 
-from decouple import config
-
 from .forms import ContactForm
+from .utils import send_email
 
 
 class ContactView(FormView):
@@ -13,20 +10,9 @@ class ContactView(FormView):
     success_url = '/success/'
 
     def form_valid(self, form):
+        form.save()
+
         # Send a confirmation email with contact form data
-        subject = 'Django blog task'
-        message = f'Name: {form.cleaned_data["name"]}\nContent: {form.cleaned_data["content"]}'
-        from_email = settings.DEFAULT_FROM_EMAIL
-        recipient_list = [config('RECIPIENT')]
-
-        msg = EmailMessage(
-            subject,
-            message,
-            from_email,
-            recipient_list,
-            reply_to=[form.cleaned_data["email"]],
-        )
-
-        msg.send()
+        send_email("Django blog task", form)
 
         return super().form_valid(form)
